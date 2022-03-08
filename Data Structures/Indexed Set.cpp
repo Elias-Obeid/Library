5d9bfd834eab0e3852975c6f3c74385eae525d78
Elias Obeid <elias.obeid9.02@gmail.com>
2022-03-08 02:18:34 -0500

    Update Indexed Set.cpp

--- Data Structures/Indexed Set.cpp
+++ Data Structures/Indexed Set.cpp
@@ -6,17 +6,17 @@ using namespace std;
 using namespace __gnu_pbds;
 
 template <typename num_t>
-using indexed_multiset = tree<
-    num_t,
-    null_type,
-    less_equal<num_t>,
-    rb_tree_tag,
-    tree_order_statistics_node_update>;
-
-template <typename num_t>
 using indexed_set = tree<
     num_t,
     null_type,
     less<num_t>,
+    rb_tree_tag,
+    tree_order_statistics_node_update>;
+
+template <typename num_t>
+using indexed_multiset = tree<
+    num_t,
+    null_type,
+    less_equal<num_t>,
     rb_tree_tag,
     tree_order_statistics_node_update>;

ceada0de9b4461358f4c6329f8b676174646e7c7
Elias Obeid <elias.obeid9.02@gmail.com>
2022-04-02 05:37:06 -0400

    Update Sieve of Eratosthenes.cpp

--- Mathematics/Sieve of Eratosthenes.cpp
+++ Mathematics/Sieve of Eratosthenes.cpp
@@ -1,25 +1,27 @@
 
+const int A = 200'000;
+
 vector<bool> is_prime;
 vector<int> primes, spf;
 
-void preComputeSieve(int n)
+void preComputeSieve()
 {
-    is_prime.resize(n + 1);
-    is_prime.assign(n + 1, true);
+    is_prime.resize(A + 1);
+    is_prime.assign(A + 1, true);
 
-    spf.resize(n + 1);
+    spf.resize(A + 1);
     iota(spf.begin(), spf.end(), 0);
 
     is_prime[0] = false;
     is_prime[1] = false;
 
-    for (long long i = 2; i <= n; ++i)
+    for (long long i = 2; i <= A; ++i)
     {
         if (is_prime[i] == true)
         {
             spf[i] = i;
             primes.emplace_back(i);
-            for (long long j = i * i; j <= n; j += i)
+            for (long long j = i * i; j <= A; j += i)
             {
                 is_prime[j] = false;
                 spf[j] = min(spf[j], spf[i]);

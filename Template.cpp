9cdd933680d23ff9ff529f191db060ab92711c61
Elias Obeid <elias.obeid9.02@gmail.com>
2022-04-04 13:05:23 -0400

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -17,14 +17,12 @@ typedef long double ld;
 
 const ld EPS = 1e-9;
 const ld PIE = asin(1) * 2;
-
-const ll MAX = 100'000;
 const ll INF = 1'000'000'000LL * 2;
 
 // const int MOD = 998'244'353;
 const int MOD = 1'000'000'007;
 
-void solve_test_case()
+void solveTestCase()
 {
 
     return;
@@ -45,8 +43,7 @@ int main()
     for (int test_case_number = 1; test_case_number <= number_of_test_cases; ++test_case_number)
     {
         // cout << "Case : #" << test_case_number << "\n";
-        solve_test_case();
+        solveTestCase();
     }
-
     return 0;
 }
